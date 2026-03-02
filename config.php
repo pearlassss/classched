7b0e86c32bb111f4cb593fe46e2829ce8dbfb6fe
pearlassss <prestozamariellepearl@gmail.com>
2026-03-02 19:30:04 +0800

    Refactor config.php for improved email handling

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME', 'classs schedule db');
+define('DB_NAME', 'class schedule db');
 
 // Create connection
 $conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
@@ -131,3 +131,4 @@ function isCodeExpired($expiration) {
 }
 
 ?>
+
